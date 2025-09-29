d40de1587ac2142ee6ff3e8a589a9fa1b74a5cc7
kyomi3263-Hue <k.youmi3263@gmail.com>
2025-09-29 14:07:53 +0900

    ArticleService에 async function getArticleList() 만듦.

--- ArticleService.js
+++ ArticleService.js
@@ -1,8 +1,16 @@
-import axios from 'axios';
+import axios, { Axios } from 'axios';
+// https://panda-market-api-crud.vercel.app/docs
 
-// export async function
-// // getArticleList;()
-// // getArticle();
-// // createArticle();
-// // patchArticle();
-// // deleteArticle();
+export async function getArticleList() {
+  try {
+    const response = await axios.get(`https://`);
+    console.log(response.data);
+  } catch (error) {
+    console.error('에러메세지', error.message);
+  }
+}
+
+// getArticle();
+// createArticle();
+// patchArticle();
+// deleteArticle();
