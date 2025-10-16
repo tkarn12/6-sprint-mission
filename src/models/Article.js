134ddc613ab27c2a0b10644372231ca4a35b9501
Heong Sun <passfile2@naver.com>
2025-10-16 21:52:00 +0900

    feat: Article.js 클래스 캡슐화 및 [인기] 태그 기능 추가

--- src/models/Article.js
+++ src/models/Article.js
@@ -1,13 +1,43 @@
 export class Article {
-  constructor(title, content, writer, likeCount) {
-    this.title = title;
+  #title;
+  #likeCount;
+  constructor(title, content, writer = "익명", likeCount = 0) {
+    this.#title = title;
     this.content = content;
     this.writer = writer;
-    this.likeCount = likeCount;
+    this.#likeCount = likeCount;
     this.createdAt = new Date();
   }
 
+  get title() {
+    return this.#title;
+  }
+
+  set title(name) {
+    if (typeof name === "string") {
+      if (name !== "[인기]") {
+        this.#title = name;
+      } else {
+        console.log("[인기]는 입력할 수 없습니다.");
+      }
+    } else {
+      console.log("문자열을 입력하세요.");
+    }
+  }
+
+  get likeCount() {
+    return this.#likeCount;
+  }
+
   like() {
-    this.likeCount++;
+    this.#likeCount++;
+
+    if (this.#likeCount >= 3) {
+      if (!this.#title.includes("[인기]")) this.#title = "[인기]" + this.#title;
+    } else {
+      if (this.#title.includes("[인기]")) {
+        this.#title = this.#title.replace("[인기]", "");
+      }
+    }
   }
 }
