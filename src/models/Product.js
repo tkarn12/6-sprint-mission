d92bd8af783d6b3eeb753369f6474911d52bcc72
Heong Sun <passfile2@naver.com>
2025-10-16 14:52:25 +0900

    feat(models): implement Product class

---
+++ src/models/Product.js
@@ -0,0 +1,14 @@
+export class Product {
+  constructor(name, description, price, tags, images, favoriteCount) {
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.tags = tags;
+    this.images = images;
+    this.favoriteCount = favoriteCount;
+  }
+
+  favorite() {
+    this.favoriteCount++;
+  }
+}
