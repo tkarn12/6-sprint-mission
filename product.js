acc1fc6ca67020a69c48615a3f5e2c2fa1f02432
Yeongjun <ing08114@gmail.com>
2025-10-17 13:13:38 +0900

    product class modify

--- product.js
+++ product.js
@@ -11,13 +11,13 @@ export class Product {
 
   #favoriteCount;
 
-  constructor(name, description, price, tags, images, favoriteCount) {
+  constructor(name, description, price, tags, images, favoriteCount = 0) {
     this.#name = name;
     this.#description = description;
     this.#price = price;
     this.#tags = tags;
     this.#images = images;
-    this.#favoriteCount = 0;
+    this.#favoriteCount;
   }
 
   get name() {
